57cedd13bd19fadd5b90dd1c3272403265fdb0e7
timbad <itim.mohamed@gmail.com>
2016-08-02 19:27:16 +0100

    "Views test"

--- polls/tests.py
+++ polls/tests.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.core.urlresolvers import reverse
 
 from .models import Question
 
@@ -36,4 +37,30 @@ class QuestionMethodeTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=1)
         recent_question = Question(pub_date=time)
         self.assertEqual(recent_question.was_published_recently(), True)
+
+
+def create_question(question_text, days):
+    """
+    Creates a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    :param question_text:
+    :param days:
+    :return: Question
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionViewTest(TestCase):
+
+    def test_index_view_with_no_question(self):
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response.content, "No polls are available.")
+        self.assertQuerysetEqual(response.context("latest_question_list"), [])
+
+
+
+
 
--- polls/views.py
+++ polls/views.py
@@ -1,31 +1,33 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import  HttpResponseRedirect
 from .models import Question, Choice
-from django.template import loader
 from django.core.urlresolvers import reverse
-from django.views import generic 
+from django.views import generic
+from django.utils import timezone
 # Create your views here.
+
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
-    contexte_object_name = "latest_question_list"
+    context_object_name = "latest_question_list"
 
     def get_queryset(self):
 
         """
-        return the latest five question
+        Return the last five published questions (not including those set to be
+        published in the future).
         """
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lt=timezone.now()).order_by("-pub_date")[:5]
+
 
 class DetailView(generic.DetailView):
     template_name = "polls/detail.html"
     model = Question 
 
+
 class ResultsView(generic.DetailView):
     template_name = "polls/results.html"
     model = Question 
-    
-
 
 
 def vote(request, question_id):
